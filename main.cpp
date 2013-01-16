c13fd9bf2721697cc03c54c3a5ad2fa56857d17d
Jeong Kwang Hyeon <sivistys@gmail.com>
2013-01-16 16:36:43 +0900

    Add all

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
-#include "SFML/Graphics.hpp"
-#include "SFML/Window.hpp"
+#include <SFML/Graphics.hpp>
+#include "character.h"
 
-int main(){
-    sf::RenderWindow Window;
+int main(void){
+	sf::RenderWindow window(sf::VideoMode(800,600), "SFML");
+	Character character(0.0, 0.0);
 
-    Window.setFramerateLimit(60);
+	while(window.isOpen()){
+		sf::Event Event;
+
+		while (window.pollEvent(Event)){
+			if(Event.type == sf::Event::Closed){
+				window.close();
+			}
+
+			if(Event.type == sf::Event::KeyPressed){
+				if(Event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+		}
+
+		window.clear();
+		character.Draw(window);	
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }

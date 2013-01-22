d47d39e3415b3693b0f3e52400f326bfb87b4b68
Jeong Kwang Hyeon <sivistys@gmail.com>
2013-01-22 13:23:29 +0900

    re-refresh

--- main.cpp
+++ main.cpp
@@ -2,30 +2,30 @@
 #include "scenemanager.h"
 
 int main(void){
-        sf::RenderWindow window(sf::VideoMode(800,600), "SFML");        
-        SceneManager *sceneManager = new SceneManager();
+	sf::RenderWindow window(sf::VideoMode(800,600), "SFML");	
+	SceneManager *sceneManager = new SceneManager();
 
-        while(window.isOpen()){
-                sf::Event Event;
+	while(window.isOpen()){
+		sf::Event Event;
 
 
-                while (window.pollEvent(Event)){
-                        if(Event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-                                window.close();
-                        }
-                }
+		while (window.pollEvent(Event)){
+			if(Event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+				window.close();
+			}
+		}
 
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)){
-                                sceneManager->setScene(new GameScene());
-                }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)){
-                                sceneManager->setScene(new TitleScene());
-                }
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)){
+				sceneManager->setScene(new GameScene());
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)){
+				sceneManager->setScene(new TitleScene());
+		}
 
-                window.clear();
-                sceneManager->update();
-                sceneManager->draw(window);
-                window.display();
-        }
-        return EXIT_SUCCESS;
+		window.clear();
+		sceneManager->update();
+		sceneManager->draw(window);
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }

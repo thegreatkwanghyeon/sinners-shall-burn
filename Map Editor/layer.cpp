714c500889991ab66d935e618f84b3832a773704
SungMin Woo <dntjdals0513@gmail.com>
2013-02-12 19:28:02 +0900

    binary file save

--- Map Editor/layer.cpp
+++ Map Editor/layer.cpp
@@ -22,9 +22,16 @@ void Layer::draw(sf::RenderWindow &window){
 }
 
 void Layer::save(){
-	saveMap = fopen(saveName.c_str(), "w");
+	saveMap = fopen(saveName.c_str(), "wb");
 	for(unsigned int i=0;i<putter.getMap().size();i++){
-		fprintf(saveMap, "%f\t%f\t%d\t%d\t%d\t%d\n%s\n", putter.getMap()[i].sprite.getPosition().x, putter.getMap()[i].sprite.getPosition().y, putter.getMap()[i].sprite.getTextureRect().left, putter.getMap()[i].sprite.getTextureRect().width, putter.getMap()[i].sprite.getTextureRect().top, putter.getMap()[i].sprite.getTextureRect().height, putter.getMap()[i].string.c_str());
+		//fprintf(saveMap, "%f\t%f\t%d\t%d\t%d\t%d\n%s\n", putter.getMap()[i].sprite.getPosition().x, putter.getMap()[i].sprite.getPosition().y, putter.getMap()[i].sprite.getTextureRect().left, putter.getMap()[i].sprite.getTextureRect().width, putter.getMap()[i].sprite.getTextureRect().top, putter.getMap()[i].sprite.getTextureRect().height, putter.getMap()[i].string.c_str());
+		fwrite(&putter.getMap()[i].sprite.getPosition().x, 1, sizeof(float), saveMap);
+		fwrite(&putter.getMap()[i].sprite.getPosition().y, 1, sizeof(float), saveMap);
+		fwrite(&putter.getMap()[i].sprite.getTextureRect().left, 1, sizeof(int), saveMap);
+		fwrite(&putter.getMap()[i].sprite.getTextureRect().width, 1, sizeof(int), saveMap);
+		fwrite(&putter.getMap()[i].sprite.getTextureRect().top, 1, sizeof(int), saveMap);
+		fwrite(&putter.getMap()[i].sprite.getTextureRect().height, 1, sizeof(int), saveMap);
+		fwrite(&putter.getMap()[i].string, 1, sizeof( putter.getMap()[i].string.size() ), saveMap);
 	}
 	fclose(saveMap);
 }

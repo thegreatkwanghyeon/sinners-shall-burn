c787833de48f048cc4af5e1e5198b086a54c74d0
include42 <dlrjsgml42@naver.com>
2013-03-09 22:17:36 +0900

    XML in Battle

--- battle.cpp
+++ battle.cpp
@@ -1,8 +1,26 @@
 #include "battle.h"
-#include "stdio.h"
+#include <string.h>
 
 Battle::Battle(){
+	int i;
 	puzzle = new Puzzle();
+
+	stats.LoadFile("xmls/Monster.xml");
+	font.loadFromFile("font/spike.ttf");
+	TiXmlNode *pNode = stats.FirstChildElement("Monster")->FirstChildElement("Enemy");
+
+	for(i=0;i<=3;i++){
+		list[i].name.setString(MTW(pNode->ToElement()->GetText()));		
+		pNode->ToElement()->Attribute("hp",&list[i].status[0]);
+
+		printf("hp : %d\n",list[i].status[0]);
+
+		list[i].name.setFont(font);
+		list[i].name.setString(MTW(pNode->ToElement()->GetText()));
+		list[i].name.setColor(sf::Color(255,255,255,255));
+		list[i].name.setPosition(400.0f,400.0f+(30*i));
+		pNode = pNode->NextSibling();
+	}	
 }
 
 void Battle::update(sf::Event &event){
@@ -11,4 +29,8 @@ void Battle::update(sf::Event &event){
 
 void Battle::draw(sf::RenderWindow &window){
 	puzzle->draw(window);
+	window.draw(list[0].name);
+	window.draw(list[1].name);
+	window.draw(list[2].name);
+	window.draw(list[3].name);
 }
--- battle.h
+++ battle.h
@@ -2,11 +2,33 @@
 #define __battle__
 
 #include "scenebase.h"
+#include "tinyxml.h"
 #include "puzzle.h"
+#include "global.h"
 //#include "player.h"
+
+enum Stat{
+	hp,//체력 
+	attack,//공격력
+	defense,//방어력
+	critical,//치명타율
+	agility,//공격속도
+	accuracy,//명중율
+};
+
+struct Monster{
+	sf::Text name;
+	//char name[100];
+	int status[7];//한칸은 여유분
+};
 
 class Battle : public SceneBase{
 	private:
+		TiXmlDocument stats;
+		sf::Font font;
+
+		Monster list[100];
+
 		//Character *character;
 		Puzzle *puzzle;
 

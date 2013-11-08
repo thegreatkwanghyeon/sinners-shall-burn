14035f450bb323cf73ba6d44910273da32865268
SungMin Woo <dntjdals0513@gmail.com>
2013-11-08 18:45:28 +0900

    Added lightning color setting method

--- lightning.cpp
+++ lightning.cpp
@@ -1,19 +1,41 @@
 #include "lightning.h"
 
-Lightning::Lightning(Vector2i startPosition, Vector2i endPosition, float thickness, float detail, float displacement){
-	this->detail = detail;
-	this->displacement = displacement;
-	this->thickness = thickness;
-
-	this->startPosition = startPosition;
-	this->endPosition = endPosition;
-
+Lightning::Lightning(){
 	random = new Well512();
+}
+
+void Lightning::setStartPosition(float x, float y){
+	startPosition.x = x;
+	startPosition.y = y;
+}
+
+void Lightning::setEndPosition(float x, float y){
+	endPosition.x = x;
+	endPosition.y = y;
+}
+
+void Lightning::setDetail(float detail){
+	this->detail = detail;
+}
+
+void Lightning::setThcikness(float thickness){
+	this->thickness = thickness;
+}
+
+void Lightning::setDisplacement(float displacement){
+	this->displacement = displacement;
+}
+
+void Lightning::setColor(int r, int g, int b){
+	this->r = r;
+	this->g = g;
+	this->b = b;
 }
 
 void Lightning::drawLightning(RenderWindow &window, Vector2i startPosition, Vector2i endPosition, float displace){
 	if (displace < detail){
 		Line line;
+		line.setColor(r, g, b);
 		line.setStartPosition(startPosition.x, startPosition.y);
 		line.setEndPosition(endPosition.x, endPosition.y);
 		line.setThickness(thickness);
@@ -24,13 +46,13 @@ void Lightning::drawLightning(RenderWindow &window, Vector2i startPosition, Vect
 		float mid_x = ((startPosition.x + endPosition.x) / 2) + (random->NextFloat(-0.5, 0.5)) * displace;
 		float mid_y = ((startPosition.y + endPosition.y) / 2) + (random->NextFloat(-0.5, 0.5)) * displace;
 
-		drawLightning(window, startPosition, Vector2i(mid_x, mid_y), displace/2);
+		drawLightning(window, startPosition, Vector2i(mid_x, mid_y), displace / 2);
 		drawLightning(window, endPosition, Vector2i(mid_x, mid_y), displace / 2);
 	}
 }
 
 void Lightning::update(){
-	
+
 }
 
 void Lightning::draw(RenderWindow &window){
